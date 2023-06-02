d149a5e4fa6b30156f8822956ee135e3807f9d1b
lamtota40 <lamtota40@gmail.com>
2023-06-02 14:21:18 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+
+#ubuntu server 64bit 
+file="https://releases.ubuntu.com/focal/ubuntu-20.04.5-live-server-amd64.iso"
+
+wget -O ubuntu.iso $file
 sudo mkdir /boot/customiso
 sudo wget -N -P /etc/grub.d/ https://raw.githubusercontent.com/lamtota40/install-os-without-cd-usb/main/40_custom
 sudo update-grub
